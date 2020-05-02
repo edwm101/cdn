fd82a1b35e62c7345693df0c175625abdeb2b5fe
Med amine Bn's <bensassi.medamine@gmail.com>
2020-05-02 13:28:07 +0100

    Go

---
+++ dist/5ca0092bd60665bc0dea.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{484:function(t,e,n){"use strict";n.r(e);var l={middleware:"authenticated"},c=n(35),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("setting")])}),[],!1,null,null,null);e.default=component.exports}}]);
--- dist/e592dca759e86e3bfbe3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{488:function(t,e,n){"use strict";n.r(e);var l={middleware:"authenticated"},c=n(35),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("setting")])}),[],!1,null,null,null);e.default=component.exports}}]);
